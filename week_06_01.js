57f326ade55791a204a6ad9238ee02e2ffc8f86a
Kenya Shields <kenyashields@Kenyas-MacBook-Pro.local>
2017-04-04 02:52:04 -0500

    week_06_01.js exercise 3 answers

--- week_06_01.js
+++ week_06_01.js
@@ -16,3 +16,19 @@ var character2 = {
   occupation: 'superhero',
   age: 75}
 console.log(character2)
+
+// Exercise 3
+
+var myChurch = {
+  name: 'Grace Church of Humble',
+  address: '7224 N. Sam Houston Pkwy E., Humble, TX 77396',
+  pastor1: 'Brett Jones',
+  pastor2: 'Scott Jones',
+  membership: 6000}
+console.log(myChurch)
+console.log('I attend ' + myChurch.name + ', which is located at ' + myChurch.address + '.' + ' The pastors are two brothers named ' + myChurch.pastor1 + ' and ' + myChurch.pastor2 + '.' + ' Grace Church currently has ' + myChurch.membership + ' members.')
+
+myChurch.membership = myChurch.membership + 200
+myChurch.campuses = 4
+console.log(myChurch)
+console.log('I attend ' + myChurch.name + ', which is located at ' + myChurch.address + '.' + ' The pastors are two brothers named ' + myChurch.pastor1 + ' and ' + myChurch.pastor2 + '.' + ' Grace Church currently has ' + myChurch.membership + ' members.' + ' Grace Church has ' + myChurch.campuses + 'locations which are located in Humble, the Heights, Tomball, and Liberty.')
