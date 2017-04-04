eb8fedcda4f1b16676215aa7471d5ca21188b187
Kenya Shields <kenyashields@Kenyas-MacBook-Pro.local>
2017-04-04 02:24:09 -0500

    week_06_01.js exercise 2 answer

--- week_06_01.js
+++ week_06_01.js
@@ -1,2 +1,18 @@
 /* Exercise 1
 1. An object literal is a grouping of an object's properties and values.  This grouping can be stored in variables which saves you from retyping the same code repeatedly. */
+
+// Exercise 2
+
+var character1 = {
+  firstName: 'Mary',
+  lastName: 'Poppins',
+  occupation: 'nanny',
+  age: 53}
+console.log(character1)
+
+var character2 = {
+  firstName: 'Wonder',
+  lastName: 'Woman',
+  occupation: 'superhero',
+  age: 75}
+console.log(character2)
